fd2b520d93a0196320df43091031ae100a9f6aca
ms394 <mohsinsajan394@gmail.com>
2021-12-19 00:34:35 +0530

    Source Folder

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -4,5 +4,20 @@ contract TodoList{
     // state variable to get the no of tasks created.
     uint public taskCount = 0;
 
+    struct Task{
+        uint id;
+        string content;
+        bool isCompleted;
+    }
 
+    constructor() public{
+        createTask("Check out this todo App created using ether");
+    }
+
+    mapping(uint => Task) public tasks;
+
+    function createTask(string memory _content) public{
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    } 
 }
